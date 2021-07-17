06ac669838cfa378d99114c64c7610a3a5139068
Heitor Mariano <heitor.eumesmo@gmail.com>
2021-07-17 17:56:31 -0300

    trabalhando com threads

---
+++ src/com/loiane/cursojava/aula74/lab/SimuladorSemafaro.java
@@ -0,0 +1,15 @@
+package com.loiane.cursojava.aula74.lab;
+
+public class SimuladorSemafaro {
+
+	public static void main(String[] args) {
+		ThreadSemafaro semafaro = new ThreadSemafaro();
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println(semafaro.getCor());
+			semafaro.esperaCorMudar();
+		}
+
+		semafaro.desligarSemafaro();
+	}
+}
